2bde01110dcc6abfce991bdf946726fbdaebdbf4
dvpermyakov <dvpermyakov1@gmail.com>
2020-06-06 21:53:41 +0300

    fix logic with version

--- increment.sh
+++ increment.sh
@@ -2,18 +2,7 @@
 
 export TEMP_DIRECTORY=.tmp
 export REPOSITORY=https://github.com/dvpermyakov/public-poll-server.git
-export DEFAULT_BRANCH=feature/increase_version_2
-export FEATURE_BRANCH=feature/increase_version_2
-export COMMIT_NAME="increase version"
-export VERSION_FILE=version.txt
-
-function incrementVersion() {
-  version=$(cat $VERSION_FILE)
-  incrementedVersion="${version%.*}.$((${version##*.} + 1))"
-  cat >$VERSION_FILE <<EOF
-$incrementedVersion
-EOF
-}
+export DEFAULT_BRANCH=master
 
 function checkoutRepository() {
   git init
@@ -22,18 +11,32 @@ function checkoutRepository() {
   git fetch
   git checkout "$DEFAULT_BRANCH"
   git pull
+}
+
+function pushChangesToRepository() {
   git checkout -b "$FEATURE_BRANCH"
+  git add "$VERSION_FILE"
+  git commit -m "$COMMIT_NAME"
+  git push origin "$FEATURE_BRANCH"
 }
 
 mkdir "$TEMP_DIRECTORY"
 cd "$TEMP_DIRECTORY" || exit
 
 checkoutRepository
-incrementVersion
 
-git add $VERSION_FILE
-git commit -m "$COMMIT_NAME"
-git push origin "$FEATURE_BRANCH"
+export VERSION_FILE=version.txt
+OLD_VERSION=$(cat $VERSION_FILE)
+NEW_VERSION="${OLD_VERSION%.*}.$((${OLD_VERSION##*.} + 1))"
+
+export FEATURE_BRANCH=feature/increment_version_$NEW_VERSION
+export COMMIT_NAME="increase version $OLD_VERSION -> $NEW_VERSION"
+
+cat >$VERSION_FILE <<EOF
+$NEW_VERSION
+EOF
+
+pushChangesToRepository
 
 cd ..
 rm -rf "$TEMP_DIRECTORY"
